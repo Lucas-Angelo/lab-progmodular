1cb860e8e16193662d81847949ee0287f08582c8
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-06-20 15:48:50 -0300

    Create Combustivel enum

---
+++ trabalhofinal/src/main/java/com/abastecimento/Combustivel.java
@@ -0,0 +1,27 @@
+package com.abastecimento;
+
+public enum Combustivel {
+
+    ALCOOL(7d, 4.49d), DIESEL(4d, 4.34d), GASOLINA(10d, 5.79d);
+    
+    private double gastoMedio;
+    private double precoMedio;
+
+    private void init(double gastoMedio, double precoMedio) {
+        this.gastoMedio = gastoMedio;
+        this.precoMedio = precoMedio;
+    }
+
+    Combustivel(double gastoMedio, double precoMedio) {
+        init(gastoMedio, precoMedio);
+    }
+
+    public double consumo() {
+        return this.gastoMedio;
+    }
+
+    public double precoMedio() {
+        return this.precoMedio;
+    }
+    
+}
