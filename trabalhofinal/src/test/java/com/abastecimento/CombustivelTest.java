3cacf83eacd7a7973b655e22567ac2900db15aad
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-06-20 16:53:28 -0300

    Fix test

--- trabalhofinal/src/test/java/com/abastecimento/CombustivelTest.java
+++ trabalhofinal/src/test/java/com/abastecimento/CombustivelTest.java
@@ -12,22 +12,22 @@ public class CombustivelTest {
     @Test
     public void ALCOOLTest() {
         this.combustivel = Combustivel.ALCOOL;
-        assertEquals(7, this.combustivel.consumo(), 1.0);
-        assertEquals(4.49d, this.combustivel.precoMedio(), 1.0);
+        assertEquals(7, this.combustivel.consumo(), .0);
+        assertEquals(4.49d, this.combustivel.precoMedio(), .0);
     }
 
     @Test
     public void DIESELTest() {
         this.combustivel = Combustivel.DIESEL;
-        assertEquals(4, this.combustivel.consumo(), 1.0);
-        assertEquals(4.34d, this.combustivel.precoMedio(), 1.0);
+        assertEquals(4, this.combustivel.consumo(), .0);
+        assertEquals(4.34d, this.combustivel.precoMedio(), .0);
     }
 
     @Test
     public void GASOLINATest() {
         this.combustivel = Combustivel.GASOLINA;
-        assertEquals(10, this.combustivel.consumo(), 1.0);
-        assertEquals(5.79d, this.combustivel.precoMedio(), 1.0);
+        assertEquals(10, this.combustivel.consumo(), .0);
+        assertEquals(5.79d, this.combustivel.precoMedio(), .0);
     }
 
 }
