1b40f667b39a12fc00ab3384af468db00e814338
Marcos Felipe Batista Muniz <filips8@hotmail.com>
2021-06-20 23:18:59 -0300

    Refactor_1
    refatoracao 1

--- trabalhofinal/src/main/java/com/manutencao/Curta.java
+++ trabalhofinal/src/main/java/com/manutencao/Curta.java
@@ -3,10 +3,11 @@ package com.manutencao;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Curta implements IManutencao, Serializable {
+public class Curta implements IManutencao, Serializable,Comparable<Curta> {
 
     protected int ultimaManun;
     private static final long serialVersionUID = 100L;
+    static final int kmManutCurta = 10000;
 
     public Curta (){
         init(0);
@@ -23,33 +24,50 @@ public class Curta implements IManutencao, Serializable {
 
     @Override
     public int proximaManutencao(int kmManutencao) {
-        return  ( ultimaManun - kmManutencao );
+        return  ( this.ultimaManun - kmManutencao );
     }
 
-    public boolean executaManutenção(int kmManutencao){
+    public boolean executaManutencao(int kmManutencao){
         ultimaManun = kmManutencao;
-        return (ultimaManun == kmManutencao);
-
-
+        return (this.ultimaManun == kmManutencao);
     }
 
     @Override
     public String toString() {
-        return "Curta{" +
-                "ultimaManun= " + ultimaManun +
-                '}';
+        return "Ultima Manutencao em: "+this.ultimaManun+" km's.";
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        Longa longa = (Longa) o;
-        return ultimaManun == longa.ultimaManun;
+        try{
+            if (this == o) return true;
+            Curta curta = (Curta) o;
+            return this.ultimaManun == curta.ultimaManun;
+        }catch(ClassCastException e){
+            System.err.print("Cast inválido");
+            return false;
+        }catch (NullPointerException e){
+            System.err.print("Ponteiro Nulo");
+            return false;
+        }
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ultimaManun);
+        return Objects.hash(this.ultimaManun);
+    }
+    @Override
+    public int compareTo(Curta o) {
+        try{
+            Curta curta = (Curta) o;
+            return this.ultimaManun == curta.ultimaManun? 0 : (this.ultimaManun < curta.ultimaManun ? 1: -1);
+        }catch(ClassCastException e){
+            System.err.print("Cast inválido");
+            return -1;
+        }catch (NullPointerException e){
+            System.err.print("Ponteiro Nulo");
+            return -1;
+        }
     }
 
 }
--- trabalhofinal/src/main/java/com/manutencao/Media.java
+++ trabalhofinal/src/main/java/com/manutencao/Media.java
@@ -3,9 +3,10 @@ package com.manutencao;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Media implements IManutencao, Serializable {
+public class Media implements IManutencao, Serializable,Comparable<Media> {
     protected int ultimaManun;
-    private static final long serialVersionUID = 102L;
+    private static final long serialVersionUID = 101L;
+    private static final int kmManuteMedia = 12000;
 
     public Media (){
         init(0);
@@ -22,31 +23,51 @@ public class Media implements IManutencao, Serializable {
 
     @Override
     public int proximaManutencao(int kmManutencao) {
-        return  ( ultimaManun - kmManutencao );
+        return  ( this.ultimaManun - kmManutencao );
     }
 
-    public boolean executaManutenção(int kmManutencao){
+    public boolean executaManutencao(int kmManutencao){
         ultimaManun = kmManutencao;
-        return (ultimaManun == kmManutencao);
-
-
+        return (this.ultimaManun == kmManutencao);
     }
+
     @Override
     public String toString() {
-        return "Media{" +
-                "ultimaManun= " + ultimaManun +
-                '}';
+        return "Ultima Manutencao em: "+this.ultimaManun+" km's.";
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        Longa longa = (Longa) o;
-        return ultimaManun == longa.ultimaManun;
+        try{
+            if (this == o) return true;
+            Media media = (Media) o;
+            return this.ultimaManun == media.ultimaManun;
+        }catch(ClassCastException e){
+            System.err.print("Cast inválido");
+            return false;
+        }catch (NullPointerException e){
+            System.err.print("Ponteiro Nulo");
+            return false;
+        }
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ultimaManun);
+        return Objects.hash(this.ultimaManun);
     }
+
+    @Override
+    public int compareTo(Media o) {
+        try{
+            Media media = (Media) o;
+            return this.ultimaManun == media.ultimaManun? 0 : (this.ultimaManun < media.ultimaManun ? 1: -1);
+        }catch(ClassCastException e){
+            System.err.print("Cast inválido");
+            return -1;
+        }catch (NullPointerException e){
+            System.err.print("Ponteiro Nulo");
+            return -1;
+        }
+    }
+
 }
